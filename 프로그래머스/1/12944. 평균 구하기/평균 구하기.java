da46e6369d3d6916eeeb80c22fc0c3364ccc417d
재민 <145990859+CheatIsKey@users.noreply.github.com>
2026-05-02 10:00:01 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public double solution(int[] arr) {
+        int sum = 0;
+
+        for (int i : arr) {
+            sum += i;
+        }
+
+        return (double) sum / arr.length;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
